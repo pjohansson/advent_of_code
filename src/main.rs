c5f0ea45a5ab338129010724b6f3d8d6c283c266
Petter Johansson <pettjoha@kth.se>
2016-03-04 21:08:27 +0100

    day1: finished

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,10 @@
+mod day1;
+
 fn main() {
-    println!("Hello, world!");
+    let day1_data = day1::get_data();
+    println!("Day 1");
+    println!("  Main: {}", day1::main(day1_data) );
+    println!("  Extra: {}", day1::extra(day1_data) );
+    println!("")
 }
 
-#[test]
-fn test_day1a() {
-    struct CheckResult<'a> {
-        instructions: &'a str,
-        result: i32
-    };
-
-    let expected = [
-        CheckResult { instructions: "(())", result: 0 },
-        CheckResult { instructions: "()()", result: 0 },
-        CheckResult { instructions: "(((", result: 3 },
-        CheckResult { instructions: "(()(()(", result: 3 },
-        CheckResult { instructions: "))(((((", result: 3 },
-        CheckResult { instructions: "())", result: -1 },
-        CheckResult { instructions: "))(", result: -1 },
-        CheckResult { instructions: ")))", result: -3 },
-        CheckResult { instructions: ")())())", result: -3 },
-    ];
-
-    for check in &expected {
-        assert!(day1_a(check.instructions) == check.result);
-    }
-}
-
-fn day1_a(instructions: &str) -> i32 {
-    let mut pos = 0;
-
-    for c in instructions.chars() {
-        pos += match c {
-            '(' => 1,
-            ')' => -1,
-            _   => 0,
-        }
-    }
-
-    pos
-}
