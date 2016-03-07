d8f274f18cf465d8d561ab831a3afba37a5562cd
Petter Johansson <pettjoha@kth.se>
2016-03-07 12:12:42 +0100

    day2: main and extra finished

--- src/day2.rs
+++ src/day2.rs
@@ -1,3 +1,23 @@
+use std::fs::File;
+use std::path::Path;
+use std::io::prelude::*;
+
+pub fn read_file() -> String {
+    let filename = "example_files/day2_input.txt";
+    let path = Path::new(filename);
+
+    let mut file = File::open(path).unwrap();
+    let mut string = String::new();
+
+    // Lesson learned: File access! Must be unwrapped.
+    match file.read_to_string(&mut string) {
+        Ok(_) => (),
+        Err(msg) => println!("{}", msg),
+    };
+
+    string
+}
+
 // Lesson learned: This is another way to pass an array (specifially Vec) to a function
 fn min_array(array: Vec<i32>) -> i32 {
     // Lesson learned: `min` functions only on an iterator, and maybe not for floats
@@ -7,8 +27,7 @@ fn min_array(array: Vec<i32>) -> i32 {
     }
 }
 
-// Lesson learned: A Vec is returned like this
-fn get_box_areas(dim_string: &str) -> Vec<i32> {
+fn get_box_sides(dim_string: &str) -> Vec<i32> {
     // Lesson learned: `split` and `collect`!
     let array: Vec<&str> = dim_string.split('x').collect();
     let mut sides = Vec::new();
@@ -21,9 +40,15 @@ fn get_box_areas(dim_string: &str) -> Vec<i32> {
         }
     }
 
+    sides
+}
+
+// Lesson learned: A Vec is returned like this
+fn get_box_areas(dim_string: &str) -> Vec<i32> {
+    let sides = get_box_sides(dim_string);
+
     // Lesson learned: How to construct vectors piece by piece
     let mut areas = Vec::new();
-
     for i in 0..3 {
         for j in i..3 {
             if i != j {
@@ -35,7 +60,17 @@ fn get_box_areas(dim_string: &str) -> Vec<i32> {
     areas
 }
 
-pub fn main(dim_string: &str) -> i32 {
+pub fn main(dim_boxes_str: &str) -> i32 {
+    let mut area = 0;
+
+    for line in dim_boxes_str.lines() {
+        area += area_one_box(line);
+    }
+
+    area
+}
+
+fn area_one_box(dim_string: &str) -> i32 {
     let areas = get_box_areas(dim_string);
     // Lesson learned: `sum` on iterator is unstable
     let sum = areas[0] + areas[1] + areas[2];
@@ -43,10 +78,36 @@ pub fn main(dim_string: &str) -> i32 {
     2*sum + min_array(areas)
 }
 
+pub fn extra(dim_boxes_str: &str) -> i32 {
+    let mut length = 0;
+
+    for line in dim_boxes_str.lines() {
+        length += length_one_box(line);
+    }
+
+    length
+}
+
+fn length_one_box(dim_string: &str) -> i32 {
+    let sides = get_box_sides(dim_string);
+
+    let mut perimeters = Vec::new();
+    for i in 0..3 {
+        for j in i..3 {
+            if i != j {
+                perimeters.push(2*(sides[i] + sides[j]));
+            }
+        }
+    }
+
+    let volume = sides[0]*sides[1]*sides[2];
+    volume + min_array(perimeters)
+}
+
 #[cfg(test)]
 pub mod tests {
     // Lesson learned: Import with easy access from tests
-    use day1::tests::{Expected,assert_expected_answers};
+    use day1::tests::{Expected, assert_expected_answers, assert_one_expected};
     use super::*;
 
     #[test]
@@ -55,8 +116,23 @@ pub mod tests {
             Expected { input: "2x3x4",  result: 58 },
             Expected { input: "1x1x10", result: 43 },
             ];
+        let expected_double = Expected { input: "2x3x4\n1x1x10\n", result: 58+43 };
 
-        assert_expected_answers(&expected_answers, main);
+        // Lesson learned: Access private functions in `super` module by explicit signature
+        assert_expected_answers(&expected_answers, super::area_one_box);
+        assert_one_expected(&expected_double, main);
+   }
+
+    #[test]
+    fn day2b() {
+        let expected_answers = [
+            Expected { input: "2x3x4",  result: 34 },
+            Expected { input: "1x1x10", result: 14 },
+            ];
+        let expected_double = Expected { input: "2x3x4\n1x1x10\n", result: 34+14 };
+
+        assert_expected_answers(&expected_answers, super::length_one_box);
+        assert_one_expected(&expected_double, extra);
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 mod day1;
+mod day2;
 
 fn main() {
     let day1_data = day1::get_data();
     println!("Day 1");
     println!("  Main: {}", day1::main(day1_data) );
     println!("  Extra: {}", day1::extra(day1_data) );
-    println!("")
+    println!("");
+
+    let day2_data = day2::read_file();
+    println!("Day 2");
+    println!("  Main: {}", day2::main(&day2_data) );
+    println!("  Extra: {}", day2::extra(&day2_data) );
+    println!("");
 }
 
